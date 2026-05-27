56fafb1c167748d3a582a9982b72002ff6d01019
itsritesh02 <itsritesh02@gmail.com>
2026-05-27 16:53:57 +0530

    modified index.html

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,18 @@
-import Hero from "../sections/Hero"
-import About from "../sections/About";
-import Skills from "../sections/Skills";
-import Projects from "../sections/Projects";
+import About from "../Sections/About";
 import Contact from "../sections/Contact";
+import Hero from "../Sections/Hero";
+import Projects from "../Sections/Projects";
+import Skills from "../Sections/Skills";
+
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+     <Hero/>
+     <About/>
+     <Skills/>
+     <Projects/>
+     <Contact/>
     </main>
   );
 }
